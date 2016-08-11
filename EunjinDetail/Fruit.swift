442d0574a8dceeb18bec4cd74c8e908659a3e480
eunjin3786 <eunjin@eunjinui-MacBook-Air.local>
2016-08-11 14:22:02 +0900

    commit

---
+++ EunjinDetail/Fruit.swift
@@ -0,0 +1,21 @@
+//
+//  Fruit.swift
+//  EunjinDetail
+//
+//  Created by eunjin Jo on 2016. 8. 11..
+//  Copyright © 2016년 eunjin. All rights reserved.
+//
+
+import Foundation
+
+class Fruit{
+    var name:String
+    var price:Int
+    var color:String
+    
+    init(name:String,price:Int,color:String){
+        self.name = name
+        self.price = price
+        self.color = color
+    }
+}
