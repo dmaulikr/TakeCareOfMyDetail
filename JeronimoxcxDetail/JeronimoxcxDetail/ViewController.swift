40b8cc87666cb0fc97e01048ae4d7e59c8022eae
gusrn0505 <cscoi017@cscoi017ui-iMac.local>
2016-08-11 13:32:14 +0900

    GeonSun Lee Commit
    Initial Project

---
+++ JeronimoxcxDetail/JeronimoxcxDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JeronimoxcxDetail
+//
+//  Created by cscoi017 on 2016. 8. 11..
+//  Copyright © 2016년 Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
