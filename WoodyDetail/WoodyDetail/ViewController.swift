b9a7b7000b1a077c5d41d2b8ff3a3f6d39a787cc
lwoody <lwoody@korea.ac.kr>
2016-08-11 13:30:15 +0900

    Add files via upload

---
+++ WoodyDetail/WoodyDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WoodyDetail
+//
+//  Created by 이우재 on 2016. 8. 11..
+//  Copyright © 2016년 LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
