e6f3545b2b9001cb1d28ccb98f9c9641618793f7
eunjin3786 <eunjin@eunjinui-MacBook-Air.local>
2016-08-11 13:31:27 +0900

    commit

---
+++ EunjinDetail/EunjinDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EunjinDetail
+//
+//  Created by eunjin Jo on 2016. 8. 11..
+//  Copyright © 2016년 eunjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
