2d82482914b80e32639b51fabe7549ff030962bf
cscoi027 <cscoi027@cscoi027ui-iMac.local>
2016-08-11 14:40:26 +0900

    notary update

---
+++ norary3Detail/norary3Detail/Event.swift
@@ -0,0 +1,30 @@
+//
+//  Event.swift
+//  norary3Detail
+//
+//  Created by cscoi027 on 2016. 8. 11..
+//  Copyright © 2016년 Koreauniv. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    var name:String
+    var start:Time
+    var memo:String
+    
+    typealias Time = (y:Int, m:Int, d:Int, hh:Int, mm: Int)
+    
+    init (name:String) {
+        self.name = name
+        self.start = (2016,08,11,14,00)
+        self.memo = "text"
+    }
+    
+    init(name:String, start:Time) {
+        self.name = name
+        self.start = start
+        self.memo = "text"
+    }
+    
+}
---
+++ norary3Detail/norary3Detail/EventTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  EventTableViewController.swift
+//  norary3Detail
+//
+//  Created by cscoi027 on 2016. 8. 11..
+//  Copyright © 2016년 Koreauniv. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewController: UITableViewController {
+    var events:[Event] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let study = Event(name: "공부")
+        let work = Event(name: "알바", start: (2016,08,11,16,00))
+        
+        events += [study, work]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return events.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("EventCell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        
+        let currentEvent = events[indexPath.row]
+        cell.textLabel!.text = currentEvent.name
+        cell.detailTextLabel?.text = currentEvent.memo
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
