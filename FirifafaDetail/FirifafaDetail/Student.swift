385dbb3ccc7a72e4363ff96fb9693c63c8868488
firifafa <firifafa@naver.com>
2016-08-11 14:53:42 +0900

    Added Detail Variables

--- FirifafaDetail/FirifafaDetail/Student.swift
+++ FirifafaDetail/FirifafaDetail/Student.swift
@@ -9,16 +9,24 @@
 import Foundation
 
 class Student {
-    var name:String
-    var major:String
-    var grade:Int
-    var score:Double
+    var profileImage:UIImage //프로필 이미지
+    var name:String //학생이름
+    var major:String //전공
+    var admissionYear:Int //입학년도 (YYYY)
+    var grade:Int //학년 (1~4)
+    var scores:[Double] //지금까지의 학점 배열
+    var average:Double //평균학점
+    
     
     init() {
+        self.profileImage = nil
         self.name = ""
         self.major = ""
+        self.admissionYear = 2016
         self.grade = 0
-        self.score = 0.0
+        self.scores = []
+        self.average = 0.0
+        
         
     }
     
