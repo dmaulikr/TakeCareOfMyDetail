dbc5e458961c60d0068a3ae4c9b64e50fee1001c
ahsib1 <step@STEPui-MacBook-Air.local>
2016-08-11 13:59:20 +0900

    SeoulTrip
    SeoulTrip

---
+++ AhsibDetail/AhsibDetail/SeoulTrip.swift
@@ -0,0 +1,24 @@
+//
+//  SeoulTrip.swift
+//  AhsibDetail
+//
+//  Created by STEP on 2016. 8. 11..
+//  Copyright © 2016년 STEP. All rights reserved.
+//
+
+import Foundation
+
+class SeoulTrip{
+    var name:String
+    var info:String
+    var subway:Int
+    
+    init(){
+        self.name = ""
+        self.info = ""
+        self.subway = 0
+        
+    }
+    
+
+}
---
+++ AhsibDetail/AhsibDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AhsibDetail
+//
+//  Created by STEP on 2016. 8. 11..
+//  Copyright © 2016년 STEP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
