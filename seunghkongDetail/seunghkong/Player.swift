afde5ace407c84118796601dc200f4f10a03bf1c
seunghkong <seunghkong@gmail.com>
2016-08-11 14:23:31 +0900

    ?
    d

---
+++ seunghkongDetail/seunghkong/Player.swift
@@ -0,0 +1,28 @@
+//
+//  Player.swift
+//  seunghkongDetail
+//
+//  Created by Seung Hyun Kong on 8/11/16.
+//  Copyright © 2016 Seung Hyun Kong. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    let name:String
+    var position:Int
+    var average:Double
+    
+    init(name:String) {
+        self.name = name
+        self.position = 0
+        self.average = 0.0
+    }
+}
+
+/*
+    func init(name:String){
+        self.name = name
+    }
+}
+ */
---
+++ seunghkongDetail/seunghkong/PlayerTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  PlayerTableViewController.swift
+//  seunghkongDetail
+//
+//  Created by Seung Hyun Kong on 8/11/16.
+//  Copyright © 2016 Seung Hyun Kong. All rights reserved.
+//
+
+import UIKit
+
+class PlayerTableViewController: UITableViewController {
+
+    var players:Array<Player> = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let mino = Player(name: "강민호")
+        mino.position = 0
+        mino.average = 3.00
+        
+        let jongyoon = Player(name: "박종윤")
+        jongyoon.position = 0
+        jongyoon.average = 2.99
+        
+        players += [mino, jongyoon]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return players.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        let currentPlayer = players[indexPath.row]
+        cell.textLabel!.text = currentPlayer.name
+        cell.detailTextLabel?.text = String(currentPlayer.average)
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
