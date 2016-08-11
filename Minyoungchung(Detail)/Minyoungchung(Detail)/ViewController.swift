faabda6f9f24426ae2a62cfe35112d0f6ba36d86
Minyoungchung <myj9671@gmail.com>
2016-08-11 13:33:21 +0900

    Add files via upload

---
+++ Minyoungchung(Detail)/Minyoungchung(Detail)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minyoungchung(Detail)
+//
+//  Created by cscoi008 on 2016. 8. 11..
+//  Copyright © 2016년 Minyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
