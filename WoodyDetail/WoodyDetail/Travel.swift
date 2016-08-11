a7f9957a79deba1311caf9df02ef2cb2927e963c
lwoody <lwoody@korea.ac.kr>
2016-08-11 14:24:19 +0900

    fdsa

---
+++ WoodyDetail/WoodyDetail/Travel.swift
@@ -0,0 +1,120 @@
+//
+//  Player.swift
+//  WoodyDetail
+//
+//  Created by 이우재 on 2016. 8. 11..
+//  Copyright © 2016년 LEE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Item {
+    
+    var price : Double
+    var currency : String
+    var pay : String
+    var category : String
+    var date : String // 어떤 타입?
+    var photo : UIImage?
+    
+    init(_ _price:Double, _ _currency:String, _ _pay:String, _ _category:String, _ _date:String ){
+        
+        price = _price
+        currency = _currency
+        pay = _pay
+        category = _category
+        date = _date
+        
+    }
+    
+    
+    
+}
+
+class TravelWhere {
+    
+    var title : String
+    var period : Double // 기간 어떤타입?? 데이트피커에서 받아와야함
+    
+    var initCardBudget:Double = 0
+    var initCashBudget:Double = 0
+    var initTotalBudget:Double = 0
+    var plan : String?
+    var items : [Item]?
+    
+    init(_ _title:String, _ _period:Double, _ _cardBudget:Double, _ _cashBudget:Double){
+        
+        title  = _title
+        period = _period
+        initCardBudget = _cardBudget
+        initCashBudget = _cashBudget
+        initTotalBudget = _cardBudget + _cashBudget
+    }
+    
+    // 여행안에 item들이 존재한다면 쓴돈, 남은돈 계산해주는 함수
+    
+    // 1. 지불수단별로 아이템들 계산하여 카드쓴돈, 현금쓴돈, 모두쓴돈, 카드남은돈, 현금쓴돈, 모두남은돈 리턴
+    
+    func PayByBudget() -> (cardSpend:Double, cashSpend:Double, totalSpend:Double, cardRemian:Double, cashRemain:Double, totalRemain:Double){
+        
+        var card:Double = 0
+        var cash:Double = 0
+        
+        
+        if let items = items{
+            let cardContent = items.filter({ $0.pay == "card" })
+            
+            for i in cardContent{
+                card += i.price
+            }
+            
+            let cashContent = items.filter({ $0.pay == "cash" })
+            
+            for i in cashContent{
+                cash += i.price
+            }
+        }
+        
+        let cardremain = initCardBudget-card
+        let cashremain = initCashBudget-cash
+        let totalspend = card + cash
+        let totalremain = cardremain + cashremain
+        
+        return (card,cash,totalspend,cardremain,cashremain,totalremain)
+        
+    }
+    
+    
+    // 2. 화폐단위별로 아이템들 계산해주는 함수
+    
+    
+    // 3. 지불수단별로 아이템들 분류한 배열을 리턴하는 함수
+    
+    func itemsByPay() -> (cardItems:[Item], cashItems:[Item]){
+        
+        var card:[Item] = []
+        var cash:[Item] = []
+        
+        if let items = items{
+            let carditems = items.filter({ $0.pay == "card" })
+            card = carditems
+            
+            let cashitems = items.filter({ $0.pay == "cash" })
+            cash = cashitems
+            
+        }
+        
+        return(card,cash)
+    }
+    
+    // 4. 날짜별로 아이템 분류하는 배열 리턴하는 함수
+    
+    // 5. 데이트피커로 받은 데이터값을 이용해 년,월,일로 나누어 print해주는 함수
+    // 비용추가페이지에서 추가시킬 날짜를 선택하도록 하고 보여주는 페이지는 전체 목록을 보여주기(비용추가에서 날짜선택 기본값은 전에 입력했던 값으로 해노음)
+    
+    
+}
+
+
+    
---
+++ WoodyDetail/WoodyDetail/TravelTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  TravelTableViewController.swift
+//  WoodyDetail
+//
+//  Created by 이우재 on 2016. 8. 11..
+//  Copyright © 2016년 LEE. All rights reserved.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        let japanItem1 = Item(1000, "yen", "card", "shopping", "2016-08-01")
+        let japanItem2 = Item(2000, "yen", "cash", "sleeping", "2016-08-02")
+        let japanItem3 = Item(3000, "won", "card", "moving", "2016-08-03")
+        
+        let japanTravel = TravelWhere("Japan", 2016.08 , 4000, 3000)
+        japanTravel.plan = "일본여행입니다"
+        japanTravel.items = [japanItem1,japanItem2,japanItem3]
+      
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return japanTravel.items.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TravelCell", forIndexPath: indexPath)
+        
+        let item = japanTravel.items[indexPath]
+        // Configure the cell...
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
