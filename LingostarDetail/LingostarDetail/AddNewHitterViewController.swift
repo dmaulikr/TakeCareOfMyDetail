628188ff951e177613dde1b69ec548ef9a003aa8
Seongkwan Yoon <admin@lingostar.co.kr>
2016-08-22 11:13:05 +0900

    ViewLifeCycle

--- LingostarDetail/LingostarDetail/AddNewHitterViewController.swift
+++ LingostarDetail/LingostarDetail/AddNewHitterViewController.swift
@@ -50,7 +50,36 @@ class AddNewHitterViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+    }
 
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
